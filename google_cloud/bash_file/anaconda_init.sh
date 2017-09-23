d93a7f38f77d52f1482d468a394d53184aecf064
pizza12333 <pizza12333@naver.com>
2017-09-23 11:59:03 +0900

    bash_file

--- google_cloud/bash_file/anaconda_init.sh
+++ google_cloud/bash_file/anaconda_init.sh
@@ -2,8 +2,8 @@ sudo apt-get gcc -y
 sudo apt-get g++ -y
 sudo apt-get make -y
 
-mkdir downloads
-cd downloads
+mkdir /home/supermario0759/downloals
+cd /home/supermario0759/downloads
 
 wget https://repo.continuum.io/archive/Anaconda2-4.4.0-Linux-x86_64.sh
 bash Anaconda2-4.4.0-Linux-x86_64.sh
